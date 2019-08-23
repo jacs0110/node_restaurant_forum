d3acd6254b2583174f9d0dd6f08ff7afdd482e9d
jacs <wbruce0110@gmail.com>
2019-08-23 09:47:01 +0800

    add navbar

--- app.js
+++ app.js
@@ -21,16 +21,16 @@ app.use(session({
   resave: false,
   saveUninitialized: false
 }))
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(flash())
+
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
   res.locals.error_messages = req.flash('error_messages')
+  res.locals.user = req.user
   next()
 })
-
-app.use(passport.initialize())
-app.use(passport.session())
-
 
 app.listen(port, () => {
   db.sequelize.sync()
