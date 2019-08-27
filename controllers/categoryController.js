3ce6fe7075f03e38dbc053985ff44973e69256ce
jacs <wbruce0110@gmail.com>
2019-08-27 12:44:48 +0800

    add admin categories post feature

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -6,6 +6,19 @@ let categoryController = {
     return Category.findAll().then(categories => {
       return res.render('admin/categories', { categories: categories })
     })
+  },
+
+  postCategory: (req, res) => {
+    if (!req.body.name) {
+      req.flash('error_messages', 'Please enter a name!')
+      return res.redirect('back')
+    } else {
+      return Category.create({
+        name: req.body.name
+      }).then(category => {
+        res.redirect('/admin/categories')
+      })
+    }
   }
 }
 
