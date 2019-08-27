604d7456508ad8c9f1eee3ab01043331d20ab776
jacs <wbruce0110@gmail.com>
2019-08-27 21:24:32 +0800

    add restautant show page

--- controllers/restController.js
+++ controllers/restController.js
@@ -13,6 +13,16 @@ let restController = {
         restaurants: data
       })
     })
+  },
+
+  getRestaurant: (req, res) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: Category
+    }).then(restaurant => {
+      return res.render('restaurant', {
+        restaurant: restaurant
+      })
+    })
   }
 }
 
