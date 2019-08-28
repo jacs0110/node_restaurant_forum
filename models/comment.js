49e9eb710ed0e023681dd0a90aa287e501f7d46f
jacs <wbruce0110@gmail.com>
2019-08-28 10:20:54 +0800

    create comment model and add association

---
+++ models/comment.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Comment = sequelize.define('Comment', {
+    text: DataTypes.STRING,
+    UserId: DataTypes.INTEGER,
+    RestaurantId: DataTypes.INTEGER
+  }, {});
+  Comment.associate = function (models) {
+    // associations can be defined here
+    Comment.belongsTo(models.User)
+    Comment.belongsTo(models.Restaurant)
+  };
+  return Comment;
+};
