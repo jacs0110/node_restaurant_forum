86ff9d7998d3d0f1b38dcca7e036a4354a618ebc
jacs <wbruce0110@gmail.com>
2019-08-28 23:43:44 +0800

    add user profile page, edit profile page and feature

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,9 @@
 const bcrypt = require('bcrypt-nodejs')
 const db = require('../models')
+const fs = require('fs')
 const User = db.User
+const imgur = require('imgur-node-api')
+const IMGUR_CLIENT_ID = '9eed8735c675a97'
 
 const userController = {
   signUpPage: (req, res) => {
@@ -46,6 +49,67 @@ const userController = {
     req.flash('success_messages', "Log out successfully!")
     req.logout()
     res.redirect('/signin')
+  },
+
+  getUser: (req, res) => {
+    let isSameUser = false
+    if (req.user.id === Number(req.params.id)) {
+      isSameUser = true
+    }
+    return User.findByPk(req.params.id).then(user => {
+      return res.render('user', { targetUser: user, isSameUser: isSameUser })
+    })
+  },
+
+  editUser: (req, res) => {
+    if (req.user.id === Number(req.params.id)) {
+      return User.findByPk(Number(req.params.id)).then(user => {
+        return res.render('userEdit', { user: user })
+      })
+    } else {
+      req.flash('error_messages', "You don't have the right to edit!")
+      return res.redirect(`/users/${req.params.id}`)
+    }
+  },
+
+  putUser: (req, res) => {
+    const { file } = req
+    console.log(file)
+    console.log('file above')
+    if (file) {
+      imgur.setClientID(IMGUR_CLIENT_ID)
+      imgur.upload(file.path, (err, img) => {
+        return User.findByPk(req.params.id)
+          .then((user) => {
+            user.update({
+              name: req.body.name,
+              email: req.body.email,
+              password: user.password,
+              isAdmin: user.isAdmin,
+              image: file ? img.data.link : user.image,
+            }).then((user) => {
+              // console.log(user)
+              req.flash('success_messages', 'User has been updated successfully !')
+              res.redirect(`/users/${user.id}`)
+            })
+          })
+      })
+    } else {
+      return User.findByPk(req.params.id)
+        .then((user) => {
+          user.update({
+            name: req.body.name,
+            email: req.body.email,
+            password: user.password,
+            isAdmin: user.isAdmin,
+            image: user.image,
+          }).then(user => {
+            // console.log(user)
+            req.flash('success_messages', 'User has been updated successfully !')
+            res.redirect(`/users/${user.id}`)
+          })
+        })
+    }
   }
 }
 module.exports = userController
--- seeders/20190823080631-my-seed-file.js
+++ seeders/20190823080631-my-seed-file.js
@@ -8,6 +8,7 @@ module.exports = {
       password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
       isAdmin: true,
       name: 'root',
+      image: null,
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
@@ -15,6 +16,7 @@ module.exports = {
       password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
       isAdmin: false,
       name: 'user1',
+      image: null,
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
@@ -22,6 +24,7 @@ module.exports = {
       password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
       isAdmin: false,
       name: 'user2',
+      image: null,
       createdAt: new Date(),
       updatedAt: new Date()
     }], {})
