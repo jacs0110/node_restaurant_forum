b2c9ed70582878d5fb16e6bdecee35d2143ba95a
jacs <wbruce0110@gmail.com>
2019-08-30 17:18:11 +0800

    create feeds page and feature

--- routes/index.js
+++ routes/index.js
@@ -27,6 +27,7 @@ module.exports = (app, passport) => {
     res.redirect('restaurants')
   })
   app.get('/restaurants', authenticated, restController.getRestaurants)
+  app.get('/restaurants/feeds', authenticated, restController.getFeeds)
   app.get('/restaurants/:id', authenticated, restController.getRestaurant)
 
   app.get('/admin', authenticatedAdmin, (req, res) => {
